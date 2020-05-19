c61cacb392eff614d6e0d0be09d02042a9eecc67
Vixtor61 <00198117@uca.edu.sv>
2020-05-19 16:47:22 -0600

    set message using memory

--- TL3/labo3.asm
+++ TL3/labo3.asm
@@ -4,6 +4,14 @@ section .text
 
 	call 	texto	
 	call 	cursor
+	mov	ax, msg; message
+	mov	[200h], ax
+	mov	ax, 50d;columna
+	mov	[210h], ax
+	mov	ax, 0d;fila
+	mov	[220h], ax
+	mov	ax, len;lenght
+	mov	[230h], ax
 	call 	phrase
 	call	kbwait
 
@@ -35,18 +43,20 @@ kbwait: mov 	ax, 0000h
 
 m_cursr:mov 	ah, 02h
 	mov 	dx, di  ; columna
-	add	dl, 29d
-	mov 	dh, 12d ; fila
+	add	dl, [210h]
+	mov 	dh, [220h] ; fila
 	mov 	bh, 0h
 	int 	10h
 	ret
 
 phrase:	mov 	di, 0d
-lupi:	mov 	cl, [msg+di]
+	
+lupi:	mov	bx, [200h]
+	mov	cl, [bx+di]
 	call    m_cursr
 	call 	w_char
 	inc	di
-	cmp 	di, len
+	cmp 	di, [230h]
 	jb	lupi
 	ret
 
@@ -54,7 +64,7 @@ lupi:	mov 	cl, [msg+di]
 section .data
 msg	db 	"Mami que tu quiere? "
 len 	equ	$-msg
-msg	db 	"Mi waifu >>>>> tu waifu"
-len 	equ	$-msg
-msg	db 	"Caliente Caliente"
-len 	equ	$-msg
+msg1	db 	"Mi waifu >>>>> tu waifu"
+len1 	equ	$-msg1
+msg2	db 	"Caliente Caliente"
+len2 	equ	$-msg2
